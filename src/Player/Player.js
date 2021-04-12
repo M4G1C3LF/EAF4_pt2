bca34d44f9a89dd92a3b69d6123db3cfa2220ac9
M4G1C3LF <sbalaguercarmona@gmail.com>
2021-04-12 15:03:31 +0200

    Refactoring keyHandler

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,17 +1,11 @@
 import Phaser from 'phaser';
-import spriteSheet from './sprites/player.png';
-
 class Player extends Phaser.GameObjects.Sprite{
     constructor (config) {
-        console.log("constructor!");
         super(config.scene, config.x, config.y, config.key);
         config.scene.physics.world.enable(this);
         config.scene.add.existing(this);
-        console.log("Added to scene!");
-
-     
     }
-    //Method that store the inputs given
+    // Method that store the inputs given
     savePreviousKeyInput(keys){
         this.prevKeys = { 
             left: keys.left.isDown ,
@@ -19,53 +13,57 @@ class Player extends Phaser.GameObjects.Sprite{
             crouch: keys.crouch.isDown
         } 
     }
-
-    preload () {
-        console.log("preload!");
-    }
-      
-    create () {
-        console.log("create!");
-
-        this.anims.create({
-            key: 'idle',
-            frames: [
-                {
-                    key: 'idle1',
-                    sourceIndex: "player/idle1",
-                    frame: 0
-                },
-                {
-                    key: 'idle2',
-                    sourceIndex: "player/idle2",
-                    frame: 1
-                }
-            ],
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.player.play("idle");
-
-    }
-
-    update(scene) {
-        const {keys} = scene;
-
-        //If this is the first frame it will just take the keys pressed
+    // Method to handle user's input
+    keyHandler(scene){
+        const { keys } = scene;
+        // If this is the first frame it will just take the keys pressed and return.
         if (!this.prevKeys){
             this.savePreviousKeyInput(keys);
             return;
         }
 
+        // ON PRESS LEFT
+        if(keys.left.isDown && !this.prevKeys.left){
+            scene.player.flipX = true;
+            if(!keys.crouch.isDown){
+                scene.player.play("move");
+            }
+        }
+        // ON RELEASE LEFT
+        if(!keys.left.isDown && this.prevKeys.left && !keys.crouch.isDown ){
+            scene.player.play("idle");
+        }
+        // ON PRESS RIGHT
+        if(keys.right.isDown && !this.prevKeys.right){
+            scene.player.flipX = false;
+            if(!keys.crouch.isDown){
+                scene.player.play("move");
+            }
+        }
+        // ON RELEASE RIGHT
+        if(!keys.right.isDown && this.prevKeys.right && !keys.crouch.isDown){
+            scene.player.play("idle");
+        }
+        // ON PRESS DOWN
         if(keys.crouch.isDown && !this.prevKeys.crouch){
             scene.player.play("crouch");
         }
+        // ON RELEASE DOWN
         if(!keys.crouch.isDown && this.prevKeys.crouch){
-            scene.player.play("idle");
+            if (keys.left.isDown){
+                scene.player.play("move");
+            } else if (keys.right.isDown){
+                scene.player.play("move");
+            } else 
+                scene.player.play("idle");
         }
-        //The last thing we do is store keys pressed on this frame.
+
+        // The last thing we do is storing keys pressed on this frame.
         this.savePreviousKeyInput(keys);
+    }
+
+    update(scene) {
+        this.keyHandler(scene);
     }
 }
 
