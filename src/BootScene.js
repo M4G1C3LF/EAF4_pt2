197bec23795df290b29ae91fe286f2a2164e92bf
M4G1C3LF <sbalaguercarmona@gmail.com>
2021-04-13 22:08:47 +0200

    Add SFX and BGM to the project

--- src/BootScene.js
+++ src/BootScene.js
@@ -44,32 +44,22 @@ class BootScene extends Phaser.Scene {
         this.load.atlas('tile', './Tile/sprites/tileSheet.png', './Tile/sprites/tileSheet.json');
         //Load Atlas to 'collectableItem' key
         this.load.atlas('collectableItem', './CollectableItem/sprites/spriteSheet.png', './CollectableItem/sprites/spriteSheet.json');
+        
+        // Load Music
+        this.load.audio('whitenoise', [
+            '../sfx/whitenoise.ogg'
+        ]);
 
+        // Load Jump
+        this.load.audio('jump', [
+            '../sfx/jump.ogg'
+        ]);
+        
+        // Load pickup
+        this.load.audio('pickUp', [
+            '../sfx/pickUp.ogg'
+        ]);
     }
 }
 
 export default BootScene;
-
-
-
-
-/*
-        // Music to play. It's not properly edited for an continous loop, but game play experience isn't really the aim of this repository either.
-        this.load.audio('overworld', [
-            'assets/music/overworld.ogg',
-            'assets/music/overworld.mp3'
-        ]);
-
-        // Sound effects in a audioSprite.
-        this.load.audioSprite('sfx', 'assets/audio/sfx.json', [
-            'assets/audio/sfx.ogg',
-            'assets/audio/sfx.mp3'
-        ], {
-            instances: 4
-        });
-
-        this.load.bitmapFont('font', 'assets/fonts/font.png', 'assets/fonts/font.fnt');
-
-        // This json contain recorded gamep
-        this.load.json('attractMode', 'assets/json/attractMode.json');
-        */
